e590dc16020c41d947a4f89428518753f80d7c60
piyushinduja <piyushinduja47@gmail.com>
2024-07-29 10:56:41 -0600

    Create React App

---
+++ facebook-messenger-clone/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+
+function App() {
+
+  const [input, setInput] = useState('')
+
+  return (
+    <div className="App">
+      <h1>Hello Everyone</h1>
+
+      <input />
+      <button>Send Message</button>
+      {/* Input Field */}
+      {/* Send Button */}
+      {/* Messages */}
+    </div>
+  );
+}
+
+export default App;
