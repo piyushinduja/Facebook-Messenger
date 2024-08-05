a973a2af164fec7bd6d0eb5f0dbf867184ef29f9
piyushinduja <piyushinduja47@gmail.com>
2024-08-05 01:03:17 -0600

    Styled Messages

--- facebook-messenger-clone/src/App.js
+++ facebook-messenger-clone/src/App.js
@@ -1,37 +1,48 @@
-import './App.css';
-import React, { useState } from 'react';
-import Button from '@mui/material/Button';
-
+import "./App.css";
+import React, { useEffect, useState } from "react";
+import { Button, FormControl, Input, InputLabel } from "@mui/material";
+import Message from "./Message";
 
 function App() {
+  const [input, setInput] = useState("");
+  const [messages, setMessages] = useState([{username:'Piyush', text:'Hello Guys'}, {username:'Akash', text:'Hey'}, {username:'Khushal', text:'Hola'}]);
+  const [username, setUsername] = useState("");
 
-  const [input, setInput] = useState('');
-  const [messages, setMessages] = useState(['Hello', 'Heyy', 'How are you']);
-
-  console.log(messages);
+  // useEffect: A react hook which allows us to run some code based on some condition
+  useEffect(()=>{
+    setUsername(prompt("Please enter your name:"))
+  }, [])
 
   const sendMessage = (event) => {
     event.preventDefault();
-    setMessages([...messages, input]);
-    setInput('');
-  }
+    setMessages([...messages, {username:username, text:input}]);
+    setInput("");
+  };
 
   return (
     <div className="App">
       <h1>Hello Everyone</h1>
-      
+      <h2>Welcome {username}</h2>
       <form>
-      
-      <input value={input} onChange={event => setInput(event.target.value)} />
-      <Button disabled={!input} variant='contained' color='primary' type='submit' onClick={sendMessage}>Send Message</Button>
+        <FormControl>
+          <InputLabel>Type your message....</InputLabel>
+          <Input value={input} onChange={(event) => setInput(event.target.value)}/>
+          <Button
+          disabled={!input}
+          variant="contained"
+          color="primary"
+          type="submit"
+          onClick={sendMessage}
+        >
+          Send Message
+        </Button>
+        </FormControl>
+        
       </form>
-      
-      {
-        messages.map(message => (
-          <p>{message}</p>
-        ))
-      }
 
+      {messages.map((message) => (
+        <Message username={message.username} text={message.text} />
+      ))}
     </div>
   );
 }
