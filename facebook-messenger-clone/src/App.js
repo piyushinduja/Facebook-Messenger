d1f6ffebe7be169c550fa0cd330ab3d3d0f086ec
piyushinduja <piyushinduja47@gmail.com>
2024-08-04 19:18:39 -0600

    Resolved hook error

--- facebook-messenger-clone/src/App.js
+++ facebook-messenger-clone/src/App.js
@@ -1,18 +1,37 @@
 import './App.css';
+import React, { useState } from 'react';
+import Button from '@mui/material/Button';
+
 
 function App() {
 
-  const [input, setInput] = useState('')
+  const [input, setInput] = useState('');
+  const [messages, setMessages] = useState(['Hello', 'Heyy', 'How are you']);
+
+  console.log(messages);
+
+  const sendMessage = (event) => {
+    event.preventDefault();
+    setMessages([...messages, input]);
+    setInput('');
+  }
 
   return (
     <div className="App">
       <h1>Hello Everyone</h1>
+      
+      <form>
+      
+      <input value={input} onChange={event => setInput(event.target.value)} />
+      <Button disabled={!input} variant='contained' color='primary' type='submit' onClick={sendMessage}>Send Message</Button>
+      </form>
+      
+      {
+        messages.map(message => (
+          <p>{message}</p>
+        ))
+      }
 
-      <input />
-      <button>Send Message</button>
-      {/* Input Field */}
-      {/* Send Button */}
-      {/* Messages */}
     </div>
   );
 }
